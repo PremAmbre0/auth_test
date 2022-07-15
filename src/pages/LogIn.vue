e05e214dd800179b5feb7fcb6de513c9a0576dc1
zoro-2002 <premambre80@gmail.com>
2022-07-15 15:09:49 +0530

    add : router added and project initilized

---
+++ src/pages/LogIn.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <button> Log in with Goggle</button>
+        <button> Log in with facebook</button>
+    </div>
+</template>
---
+++ src/pages/SignIn.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        SIGN_IN
+    </div>
+</template>
